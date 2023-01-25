c60a9dd8d16928cd436f93b33f293c4941b1dc22
root <root@LAPTOP-Q6LOO1V1>
2023-01-25 09:22:19 -0600

    initial commit

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    username: 'emilioenlaluna',
+    apiUrl: 'https://api.github.com'
+    };
---
+++ src/app/github.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from './environments/environment';
+import { User } from './user';
+import { Repository } from './repository';
+import { Organization } from './organization';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+
+  private userUrl: string = '';
+
+  constructor(private http: HttpClient) {
+    this.userUrl = `${environment.apiUrl}/users/${environment.username}`;
+  }
+
+  getUser(): Observable<User> {
+    return this.http.get<User>(this.userUrl);
+  }
+
+  getRepos(): Observable<Repository[]> {
+    return this.http.get<Repository[]>(this.userUrl + '/repos');
+  }
+  getOrganizations(): Observable<Organization[]> {
+    return this.http.get<Organization[]>(this.userUrl +'/orgs');
+    }
+}
